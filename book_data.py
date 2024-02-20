90156d0601c59f231d2e86573ca443c0e71880c9
Mouloud BELLIL <mouloud.bellil@outlook.fr>
2024-02-20 17:04:29 +0100

    Step 2 of the project is optimized: deleting unnecessary lines

--- book_data.py
+++ book_data.py
@@ -4,66 +4,51 @@ from bs4 import BeautifulSoup
 import csv
 
 BASE_URL = "https://books.toscrape.com/"
-CATALOGUE_URL = "https://books.toscrape.com/catalogue/"
 
 
 def clean_text(text):
     return text.replace('\n', '').replace('£', '').replace('../', '')
 
 
-"""def book_url_scraper(url):
-    response = requests.get(url)
-    response.encoding = 'utf-8'
-
-    if response.status_code != 200:
-        return f"Error status code {response.status_code}"
-
-    soup = BeautifulSoup(response.text, "html.parser")
-
-    return [CATALOGUE_URL + clean_text(url.find("a")["href"]) for url in soup.find_all('h3')]
-"""
-
-
 def book_information_scraper(url):
     response = requests.get(url)
+    response.encoding = response.apparent_encoding
 
     if response.status_code != 200:
-        return f"Error status code {response.status_code}"
-    response.encoding = 'utf-8'
-    soup = BeautifulSoup(response.text, 'html.parser')  # (lxml-xml, html5lib)
+        return f"Error status code: {response.status_code}"
+    soup = BeautifulSoup(clean_text(response.text), 'html.parser')  # (lxml-xml, html5lib)
 
     # Title
     title = soup.find('h1').text
 
     # Category
-    div_page_inner = soup.find_all('div', class_='page_inner')
-    li = div_page_inner[1].find('ul').find_all('li')
-    category = clean_text(li[2].text)
+    category_a = soup.find('ul').find_all('a')  # a single 'ul' in html
+    category = category_a[2].text
 
-    # UPC, Price and Availability
+    # UPC
     table_td = soup.find('table').find_all('td')
-    info_table = [clean_text(information.text) for information in table_td]
+    info_table = [information.text for information in table_td]
     universal_product_code = info_table[0]
+
+    # PRICE
     price_excluding_tax = info_table[2]
     price_including_tax = info_table[3]
-    number_available = info_table[5].split('(')[1].split(' ')[0]
 
-    # Image URL
-    image_src = soup.find('img')['src']
-    image_url = clean_text(BASE_URL + image_src)
-
-    # Description
-    article_product_page = soup.find('article', class_='product_page').find('p', recursive=False)
-    product_description = article_product_page.text
+    # Number available
+    number_available = info_table[5].split('(')[-1].split(' ')[0]
 
     # image_url
-    div_image = soup.find('div', class_='item active').find('img')
-    image_url = BASE_URL + clean_text(div_image['src'])
+    div_img = soup.find('div', class_='item active').find('img')
+    image_url = BASE_URL + div_img['src']
 
     # Review rating
     p_star = soup.find('p', class_='star-rating')
     review_rating = p_star['class'][1]
 
+    # Description
+    product_description = soup.find('article', class_='product_page').find('p', recursive=False).text
+
+    # Summary of book scraper information
     book_information = {"title": title,
                         "category": category,
                         "universal_product_code": universal_product_code,
@@ -76,16 +61,16 @@ def book_information_scraper(url):
                         "product_description": product_description
                         }
 
+    # Save scraper information in a CSV file
     with open("book_information.csv", "w", newline='') as file:
-        csv.writer(file).writerows([[information for information in book_information],
+        csv.writer(file).writerows([[header for header in book_information],
                              [information for information in book_information.values()]])
 
     return book_information
 
 
 # Call the function for a product page url
-#print(book_url_scraper(url)[0])
-url = "https://books.toscrape.com/catalogue/its-only-the-himalayas_981/index.html"
-print(book_information_scraper(url))
+print(book_information_scraper("https://books.toscrape.com/catalogue/its-only-the-himalayas_981/index.html"))
+
 
 
