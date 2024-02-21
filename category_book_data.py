b477de5813d2cd544315bf7e383bc867e0ad89cb
Mouloud BELLIL <mouloud.bellil@outlook.fr>
2024-02-21 08:57:56 +0100

    Step 3: this script retrieves URLs for a category of books

---
+++ category_book_data.py
@@ -0,0 +1,41 @@
+import requests
+from bs4 import BeautifulSoup
+from urllib.parse import urljoin
+#from book_data import clean_text
+
+BASE_URL = "https://books.toscrape.com/catalogue/"
+
+
+def clean_text(text):
+    return text.replace('\n', '').replace('£', '').replace('../', '')
+
+
+def scrap_book_category(url):
+    links_to_books = []
+    while True:
+        response = requests.get(url)
+        if response.status_code != 200:
+            return f"Error status code {response.status_code}"
+
+        soup = BeautifulSoup(clean_text(response.text), "html.parser")
+
+        # Pagination
+        title_h3 = soup.find_all("h3")
+        link_a = [link.find('a')["href"] for link in title_h3]
+
+        links_to_books.extend([BASE_URL + link for link in link_a])
+
+        next_page_element = soup.find('li', class_="next")
+
+        if not next_page_element:
+            break
+
+        next_page = next_page_element.find('a')
+        next_page_url = next_page.get('href')
+
+        url = urljoin(url, next_page_url)
+
+    return len(links_to_books)
+
+
+print(scrap_book_category("https://books.toscrape.com/catalogue/category/books/art_25/index.html"))
